44b602144f11e1834addb2b31c8ea1c95fc5ceeb
Arandi Lopez <arandilopez.93@gmail.com>
2025-12-05 14:08:04 -0600

    chore: package docs

---
+++ internal/datastructures/datastructures.go
@@ -0,0 +1,3 @@
+// Package datastructures contains implementations of common data structures such as stacks, queues, linked lists, and trees.
+// It provides generic types and methods to create and manipulate these data structures efficiently.
+package datastructures
